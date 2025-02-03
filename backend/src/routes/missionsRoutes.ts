2eea79db6d5aa7f8adb2da57dcef63afc148a4a0
Akram Hamlat <130287297+Akramh01@users.noreply.github.com>
2025-02-03 14:15:08 +0100

    Update missionsRoutes.ts

--- backend/src/routes/missionsRoutes.ts
+++ backend/src/routes/missionsRoutes.ts
@@ -5,13 +5,9 @@ const router = Router();
 
 router.get('/', getMissions);
 router.get('/id/:idM', getMissionWithId);
-
 router.get('/name/:titre', getMissionWithTitle);
-
 router.post('/add', addMission);
-
-//router.put('/update-status/:idM', updateMissionStatut);
-router.put('/update-mission-statut/:idM', updateMissionStatut);
-router.delete('/delete-mission/:idM', deleteMission);
+router.put('/update/:idM', updateMissionStatut);
+router.delete('/delete/:idM', deleteMission);
 
 export default router;
