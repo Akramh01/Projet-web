fd0c71bcb3adca7733ceabd545e33909b679bbbd
Akramh01 <akram.hamlat@univ-tlse3.fr>
2025-02-06 13:52:23 +0100

    Add bd.ts to .gitignore

--- backend/src/serveur.ts
+++ backend/src/serveur.ts
@@ -4,11 +4,17 @@ import competenceRoutes from './routes/competenceRoutes';
 import missionsRoutes from './routes/missionsRoutes';
 import { errorHandler } from './middlewares/errorHandler';
 import avoirRoutes from './routes/avoirRoutes';
+const cors = require('cors');
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+
+//enable all origins for cors 
+app.use(cors());
+
+
 
 // Routes
 app.use('/employes', employesRoutes);
