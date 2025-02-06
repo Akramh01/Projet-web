4539c11cbd383b21827e68142311b9de2a8d5df6
Akramh01 <akram.hamlat@univ-tlse3.fr>
2025-02-06 14:27:52 +0100

    message

--- backend/src/config/bd.ts
+++ backend/src/config/bd.ts
@@ -1,12 +1,16 @@
 import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
+
+// Charger les variables d'environnement depuis le fichier .env
+dotenv.config();
 
 // Connexion à la base de données
 const sequelize = new Sequelize(
   process.env.DB_NAME || 'db-web-project',
   process.env.DB_USER || 'admin',
-  process.env.DB_PASSWORD || 'Karima-0102',
+  process.env.DB_PASSWORD || 'password',
   {
-    host: process.env.DB_HOST || 'db-web-project.c7y2gyumqs5j.eu-north-1.rds.amazonaws.com',
+    host: process.env.DB_HOST || 'localhost',
     port: Number(process.env.DB_PORT) || 3306,
     dialect: 'mysql',
     logging: false, // Désactiver les logs de requêtes SQL
