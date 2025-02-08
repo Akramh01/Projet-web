69b3ea849e97a50cebba22dcfa231a2b9dd88758
Imaneqas <imane.qasbaji@toulouse.miage.fr>
2025-02-08 23:00:22 +0100

    header + début page employes

---
+++ frontend/src/app/component/collaborateur-detail/collaborateur-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-collaborateur-detail',
+  imports: [],
+  templateUrl: './collaborateur-detail.component.html',
+  styleUrl: './collaborateur-detail.component.scss'
+})
+export class CollaborateurDetailComponent {
+
+}
