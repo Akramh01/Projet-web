f7cb882229f518b75053d9f741e06d9e7793fefe
Imaneqas <imane.qasbaji@toulouse.miage.fr>
2025-02-19 11:43:34 +0100

    structure page dashboard

--- frontend/src/app/container/dashboard/dashboard.component.ts
+++ frontend/src/app/container/dashboard/dashboard.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
-
+import { DashboardStatsComponent } from "../../component/component-dashboard/dashboard-stats/dashboard-stats.component";
+import { DashboardMissionsComponent } from "../../component/component-dashboard/dashboard-missions/dashboard-missions.component";
+import { DashboardNotificationsComponent } from 'src/app/component/component-dashboard/dashboard-notifications/dashboard-notifications.component';
+import { DashboardDiagrammeComponent } from 'src/app/component/component-dashboard/dashboard-diagramme/dashboard-diagramme.component';
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [DashboardStatsComponent, DashboardMissionsComponent, DashboardNotificationsComponent, DashboardDiagrammeComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
